8c97ff08da485a1cbf29c1069809df9db77c1a0d
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-21 10:54:00 +0530

    add keyframes to animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -67,6 +67,44 @@ import { Component } from '@angular/core';
         }))
       )
     ]),
+    trigger('list2', [
+      state('in', style({
+        'opacity': '1',
+        transform: 'translateX(0)'
+      })),
+      // void means object not added to dom yet
+      // apply animation when object adding to dom
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset:0 //time of total time 
+          }),
+          style({
+            transform: 'translateX(-50x)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20x)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0x)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition(
+        '* => void',animate(300,style({
+          transform: 'translateX(100)',
+          opacity:0
+        }))
+      )
+    ])
   ]
 })
 export class AppComponent {
