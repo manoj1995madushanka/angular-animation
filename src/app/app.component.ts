77ff35573241e0a188bd3b422833444ddc015a2c
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-21 09:24:22 +0530

    shrink animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,41 @@ import { Component } from '@angular/core';
       })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(500))
+      // we can replacce above two line with welow line, but here time interval is same
+      //transition('highlighted <=> normal', animate(500))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(0)'
+      })),
+      state('shrunker', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(500)),
+      // below start mark used to represent any state
+      transition('shrunker => *', animate(300))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
+
+  onShrink(){
+    this.wildState = 'shrunker';
   }
 
   onAdd(item) {
